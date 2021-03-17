aead407f3f23dfc99549d7709842987d89179a9e
Dmitriy Strelnikov <dm.strelnikov@yandex.ru>
2021-03-17 08:53:09 +0300

    day 17

---
+++ 100-days-of-python/16-day/start-oop/main.py
@@ -0,0 +1,16 @@
+import turtle
+import another_module
+
+print (another_module.another_variable)
+
+timmy = turtle.Turtle()
+
+my_screen = turtle.Screen()
+
+print(my_screen.canvheight)
+print(timmy)
+timmy.shape("turtle")
+timmy.color("red")
+timmy.forward(200)
+
+my_screen.exitonclick()
